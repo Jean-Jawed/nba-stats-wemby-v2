390d75393c2e0a9fc30ede56214a5a06705f6b6a
Javed <jawed_tahir@yahoo.fr>
2026-02-12 18:25:12 +0100

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,15 @@ export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <Image 
-          src="/assets/images/nba.png" 
-          alt="NBA" 
+        <Image
+          src="/assets/images/nba.png"
+          alt="NBA"
           className="nba-logo"
           width={100}
           height={50}
         />
         <p>
-          By <a href="https://jawed.fr" target="_blank" rel="noopener noreferrer">Jawed</a> 2025
+          By <a href="https://javed.fr" target="_blank" rel="noopener noreferrer">Jawed</a> 2025
         </p>
       </div>
     </footer>
